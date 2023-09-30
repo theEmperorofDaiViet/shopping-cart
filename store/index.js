258c87faaba877fa119ee674b50135c4299a98fb
theEmperorofDaiViet <Khiet.To.Development@gmail.com>
2023-09-30 23:47:51 +0700

    Server API & Vuex store

---
+++ store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from 'vuex';
+import cart from './modules/cart';
+import product from './modules/product';
+
+export default createStore({
+    modules: {
+        cart,
+        product
+    }
+});

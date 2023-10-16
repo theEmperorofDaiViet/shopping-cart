3bd1814653e29b8c68335644a4e6b4e419d8b9a2
theEmperorofDaiViet <Khiet.To.Development@gmail.com>
2023-10-16 23:36:52 +0700

    support authenticated routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import CartList from '../components/cart/CartList.vue';
 import ProductList from '../components/product/ProductList.vue';
 import ProductItem from '../components/product/ProductItem.vue';
+import LoginBox from '../components/login/LoginBox.vue';
 import NotFound from '../components/NotFound.vue';
 
 const router = createRouter({
@@ -22,7 +23,29 @@ const router = createRouter({
         {
             path: "/products/:id",
             component: ProductItem,
-            props: true
+            props: true,
+            beforeEnter: (to, from, next) => {
+                const id = to.params.id;
+                if(![1, 2, 3, 4].includes(Number(id))){
+                    next('/not-found');
+                }
+                else{
+                    next();
+                }
+            },
+        },
+        {
+            path: "/login",
+            component: LoginBox,
+            beforeEnter: (to, from, next) => {
+                const token = localStorage.getItem("token");
+                if(token){
+                    next('/products');
+                }
+                else{
+                    next();
+                }
+            },
         },
         {
             path: "/:pathMatch(.*)*",
@@ -30,5 +53,15 @@ const router = createRouter({
         },
     ],
 });
+
+router.beforeEach((to, from, next) => {
+    const token = localStorage.getItem("token");
+    if(!token && to.path !== '/login'){
+        next("/login");
+    }
+    else{
+        next();
+    }
+})
 
 export default router;
